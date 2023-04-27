ef0a1852a1b7b470dd58557ca51a11e066db5876
Abrham Endale <tmailae2020@gmail.com>
2023-04-28 00:59:57 +0300

    Fixed type-checking task.

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -3,19 +3,18 @@
 
 
 from typing import Mapping, Iterable, Union, List, Tuple
-import math
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    zoomed_in: Tuple = [
+def zoom_array(lst: List, factor: int = 2) -> List:
+    zoomed_in: List = [
         item for item in lst
-        for i in range(math.floor(factor))
+        for i in range(factor)
     ]
     return zoomed_in
 
 
-array: Tuple = [12, 72, 91]
+array: List = [12, 72, 91]
 
 zoom_2x: List = zoom_array(array)
 
-zoom_3x: List = zoom_array(array, 3.0)
+zoom_3x: List = zoom_array(array, 3)

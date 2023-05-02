eb5794cd9744204fc227fa463e059f77aba08de7
Abrham Endale <tmailae2020@gmail.com>
2023-05-02 23:23:19 +0300

    Fixed measure_runtime.

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -4,11 +4,15 @@
 
 import asyncio
 import time
+
+
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 async def measure_time(n: int = 1, max_delay: int = 0) -> float:
     """Returns the average execution time."""
+    if n < 0 or max_delay < 0:
+        return (0)
     t1 = time.perf_counter()
     await wait_n(n, max_delay)
     if n > 0:

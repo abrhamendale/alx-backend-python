b3061f9ca3682cf34609a61b5e2274e2d8d90cb4
Abrham Endale <tmailae2020@gmail.com>
2023-05-02 19:32:25 +0300

    Added execute permission.

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,7 +7,7 @@ import random
 import time
 
 
-async def wait_random(max_delay = 10):
+async def wait_random(max_delay: int = 10) -> float:
     "Returns a random number between 0 and max_delay"
     r = random.uniform(0, max_delay)
     s = time.perf_counter()

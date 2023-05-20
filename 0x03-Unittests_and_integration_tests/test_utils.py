f312a3cd3165f4c214950fb0722d0a86f53baa77
Abrham Endale <abrhamendale9@gmail.com>
2023-05-20 15:08:05 +0000

    A fix for test_utils.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,6 +6,7 @@ Test module for access_nested_map
 
 import unittest
 from utils import access_nested_map
+from parameterized import parameterized
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -14,11 +15,11 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, "a", 1),
         ({"a": {"b": 2}}, "a", {"b": 2}),
-        ({"a": {"b": 2}}, "a", "b", 2)
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """Test function."""
-        assertEqual(unittest.access_nested_map(nested_map, path), expected)
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
 
 if __name__ == "__main__":

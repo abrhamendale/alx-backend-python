869825e703331f8f90346d977a3cfdc04d96b738
Abrham Endale <abrhamendale9@gmail.com>
2023-05-22 21:30:38 +0000

    Fixed pycodestyle.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -7,6 +7,7 @@ from unittest.mock import Mock, MagicMock, patch, PropertyMock
 from utils import access_nested_map, get_json, memoize
 from client import GithubOrgClient
 from parameterized import parameterized
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -15,7 +16,7 @@ class TestGithubOrgClient(unittest.TestCase):
     @parameterized.expand([
         ('google', True),
         ('abc', True)
-    ])                        
+    ])
     @patch('client.get_json')
     def test_org(self, expected, url, m_requests):
         """Tests git org."""
@@ -26,13 +27,12 @@ class TestGithubOrgClient(unittest.TestCase):
         m_requests.assert_called_once()
 
     def test_public_repos_url(self):
-        with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as p:
-            p.return_value = { "repos_url": 'abc' }
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as p:
+            p.return_value = {"repos_url": 'abc'}
             a = GithubOrgClient('google')
-            self.assertEqual(a._public_repos_url, 'abc')#'https://api.github.com/orgs/google/repos')
+            self.assertEqual(a._public_repos_url, 'abc')
 
-    #@patch('utils.get_json')
-    #def test_public_repos(self, g_mock):
     @parameterized.expand([
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False)
@@ -40,3 +40,20 @@ class TestGithubOrgClient(unittest.TestCase):
     def test_has_license(self, a, b, c):
         self.assertEqual(GithubOrgClient.has_license(a, b), c)
 
+
+"""
+@parametrized_class(('org_payload', 'repos_payload'
+, 'expected_repos', 'apache2_repos'), TEST_PATLOAD)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        with patch('client.requests') as get_patcher:
+
+    def test_public_repos(self):
+
+    def test_public_repos_with_license(self)
+
+    @classmethod
+    def tearDownClass(cls):
+"""
